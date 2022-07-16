98035b6fb0f4ec98d1e4bdac32c5d82245db9552
nada stitou <nadastito1087@gmail.com>
2022-07-16 17:10:42 +0100

    edit watsap

--- src/code-public/app/Http/Controllers/produit.php
+++ src/code-public/app/Http/Controllers/produit.php
@@ -13,6 +13,27 @@ class produit extends Controller
           ->select("*")
           ->get();
          
-          return view("pages.produits",compact("produits"));
+          return view("pages.produit",compact("produits"));
+      }
+      function afficher_detail_produit($id){
+
+       $detailProduit=DB::table('produits')
+      ->where('id_produit',$id)
+      ->select("*")
+      ->get();
+      return view("pages.detailProduit",compact("detailProduit")); 
+      }
+
+
+      function message(Request $request){
+        $nom =$request->input('nom');
+        $tele=$request->input('tele');
+        $produit=$request->input('produit');
+        $text="hello".$nom."je veux ce produit s'il vous plait".$produit;
+        if ($request->input('tele')) {
+            
+            "https://api.whatsapp.com/send/?phone=".$tele."&text=".$text."&type=phone_number&app_absent=0";
+        }
+        
       }
 }
